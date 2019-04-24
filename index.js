2ab74335c548c197b7a598c1b5043ff85fac999c
Huaichao Wang <huaichao.wang@burberry.com>
2019-04-24 14:53:12 +0800

    fill join table data using assosciation model

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const _ = require("lodash");
 const {
     sequelize,
     Survey,
@@ -30,8 +31,22 @@ sequelize.sync()
         const options2 = await bulkCreateOptions("Red", "Orange", "Yellow", "Green", "Purple", "Blue");
 
 
-        const questionOptionAssociations = await questions[0].setOptions(options1, { through: { sequenceNum: 2 } });
-        await questions[1].setOptions(options2, { through: { sequenceNum: 1 } });
+        const sequenceNumbers1 = _.shuffle(_.times(options1.length, i => i + 1));
+        const questionOptions1 = _.map(sequenceNumbers1, (num, i) => ({
+            questionId: questions[0].id,
+            optionId: options1[i].id,
+            sequenceNum: num
+        }));
+        const questionOptionAssociations = await QuestionOption.bulkCreate(questionOptions1);
+
+
+        const sequenceNumbers2 = _.shuffle(_.times(options2.length, i => i + 1));
+        const questionOptions2 = _.map(sequenceNumbers2, (num, i) => ({
+            questionId: questions[1].id,
+            optionId: options2[i].id,
+            sequenceNum: num
+        }));
+        await QuestionOption.bulkCreate(questionOptions2);
 
 
         const survey = await Survey.create({
@@ -45,15 +60,15 @@ sequelize.sync()
         await survey.addQuestion(questions[1], {
             through: {
                 sequenceNum: 90,
-                dependsOn: questionOptionAssociations[0][1].getDataValue("id"),
+                dependsOn: questionOptionAssociations[1].getDataValue("id"),
                 dependencyType: "selected"
             }
         });
-        await survey.addQuestion(questions[2], { through: { sequenceNum: 200 } });
+        await survey.addQuestion(questions[2], { through: { sequenceNum: 200, skippable: true } });
 
 
         await Submission.create({
-            questionOptionId: questionOptionAssociations[0][1].getDataValue("id"),
+            questionOptionId: questionOptionAssociations[1].getDataValue("id"),
             surveyId: survey.id,
             questionId: questions[0].id,
             freeText: "hello"
@@ -62,43 +77,50 @@ sequelize.sync()
     })
     .then(async () => {
         const surveys = await Survey.findAll({
-            include: [
-                {
-                    model: Question,
-                    include: [
-                        {
-                            model: Option
-                        }
-                    ]
-                }
-            ]
+            where: { active: true },
+            order: [
+                [Question, SurveyQuestion, "sequenceNum"],
+                [Question, Option, QuestionOption, "sequenceNum"]
+            ],
+            include: [{
+                model: Question,
+                include: [{ model: Option }]
+            }]
         });
-        const payload = surveys.map(survey => {
-            return {
+        const payload = {
+            surveys: surveys.map(survey => ({
                 id: survey.id,
                 title: survey.title,
                 language: survey.language,
-                questions: survey.questions.map(question => ({
-                    id: question.id,
-                    type: question.type,
-                    title: question.title,
-                    sequenceNum: question.sequenceNum,
-                    skippable: question.skippable,
-                    dependsOn: question.dependsOn,
-                    dependencyType: question.dependencyType,
-                    options: question.options.map(option => ({
-                        id: option.questionOption.id,
-                        title: option.title,
-                        sequenceNum: option.sequenceNum,
-                        allowFreeText: option.allowFreeText
-                    }))
-                }))
-            };
-        });
+
+                questions: survey.questions.map(question => {
+                    const DTO = {
+                        id: question.id,
+                        type: question.type,
+                        title: question.title,
+                        sequenceNum: question.surveyQuestion.sequenceNum,
+                        skippable: question.surveyQuestion.skippable,
+                        dependsOn: question.surveyQuestion.dependsOn,
+                        dependencyType: question.surveyQuestion.dependencyType
+                    };
+
+                    if (question.type !== "text") {
+                        DTO.options = question.options.map(option => ({
+                            id: option.questionOption.id,
+                            title: option.title,
+                            sequenceNum: option.questionOption.sequenceNum,
+                            allowFreeText: option.allowFreeText
+                        }));
+                    }
+
+                    return DTO;
+                })
+            }))
+        };
         console.log(JSON.stringify(payload));
     });
 
 function bulkCreateOptions(...titles) {
-    const options = titles.map((title, i) => ({ title, sequenceNum: i + 1 }));
+    const options = titles.map((title) => ({ title }));
     return Option.bulkCreate(options);
 }
